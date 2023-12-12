3acfa45a6ffc514d65dfd97bcfc438de445f5619
= <klas_hermansson@hotmail.com>
2023-12-13 00:15:51 +0100

    Part one working

--- Program.cs
+++ Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Security;
+using System.Reflection.Metadata;
+
+string[] input = File.ReadAllLines("input.txt");
+int sum = 0;
+foreach (string scratchcard in input)
+{
+    int points = 0;
+    string[] withoutCardNo = scratchcard.Split(':');
+    string[] halves = withoutCardNo[1].Split('|');
+    string[] firstHalf = halves[0].Split(' ');
+    string[] secondHalf = halves[1].Split(' ');
+
+    for (int i = 0; i < firstHalf.Length; i++)
+    {
+        if (int.TryParse(firstHalf[i], out int number))
+        {
+            for (int j = 0; j < secondHalf.Length; j++)
+            {
+                if (int.TryParse(secondHalf[j], out int secondNumber))
+                {
+                    if (firstHalf[i] == secondHalf[j])
+                    {
+                        if (points == 0)
+                        {
+                            points++;
+                        }
+                        else
+                        {
+                            points = points * 2;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    sum += points;
+}
+Console.WriteLine(sum);
+
